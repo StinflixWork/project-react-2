127fc67d0c24b074203c9107245ba33ef2ad60cf
StinflixWork <stinflix.frontsv@gmail.com>
2023-10-11 14:25:28 +0300

    update: widget temperature

---
+++ src/pages/Dashboard/DashboardWidgets/WidgetTemperature/temperatureData.data.js
@@ -0,0 +1,27 @@
+export const temperatureData = [
+	{
+		uv: 0,
+		temp: 0,
+		timeOfDay: ''
+	},
+	{
+		uv: 1,
+		temp: 11,
+		timeOfDay: 'Morning'
+	},
+	{
+		uv: 2,
+		temp: 21,
+		timeOfDay: 'Afternoon'
+	},
+	{
+		uv: 3,
+		temp: 17,
+		timeOfDay: 'Evening'
+	},
+	{
+		uv: 4,
+		temp: 12,
+		timeOfDay: 'Night'
+	}
+]
